b06ff1642eaec7bd564b4befbda06ac04ed8e902
mysnr <ilmanrls@gmail.com>
2020-12-29 23:01:36 +0700

    Mengenal Var, Let dan Const

---
+++ index.js
@@ -0,0 +1,13 @@
+var message = "Hello World";
+let message1 = "Hello";
+const message2 = "Welcome";
+
+if (true){
+    var message = "Ilma Nurlisnawati";
+    let message1 = "12 RPL";
+    console.log('hai namaku', message)
+    console.log('saya duduk di kelas', message1)
+}
+
+console.log(message)
+console.log(message1)
