71aad74e39b2c86596131c88a5e0212fadefd7d3
mysnr <ilmanrls@gmail.com>
2020-12-29 23:24:23 +0700

    Tipe Data Boolean

---
+++ boolean.js
@@ -0,0 +1,12 @@
+var ideal = "Tipe Ideal Pemain Basket"
+var tinggi = false;
+var cm = 160;
+
+if (cm < 160){
+    tinggi = true;
+} else {
+    tinggi = false;
+}
+
+console.log(ideal);
+console.log(tinggi);
