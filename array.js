55f627b13a8d11a9a003920f68c3309be4c7ad76
mysnr <ilmanrls@gmail.com>
2020-12-30 01:22:35 +0700

    Tipe Data Array

---
+++ array.js
@@ -0,0 +1,9 @@
+var ganjil = [1,3,5,7,9];
+var genap = [2,4,6,8,10];
+
+console.log('bilangan ganjil', ganjil)
+console.log('bilangan genap', genap)
+
+// untuk memanggil salah satu index pada array. tertulis di bawah sini
+console.log('index ke-3 dari ganjil', ganjil[3])
+console.log('index ke-2 dari genap', genap[2])
